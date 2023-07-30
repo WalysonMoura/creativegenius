0e7deaee247cb7909b80712112d0be0cbff03f24
{ Walyson } <walysonmoura222@gmail.com>
2023-07-30 16:24:26 -0300

    new

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -1,4 +1,3 @@
-
 import TypewriterComponent from "typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
@@ -6,11 +5,30 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
 export function LandingHero() {
-    return(
-        <div className="text-white font-bold py-36 text-center space-y-5">
-            <div>
-                <h1>The </h1>
-            </div>
+    const { isSignedIn } = useAuth();
+  return (
+    <div className="text-white font-bold py-36 text-center space-y-5">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+        <h1>A melhor ferramenta de IA para </h1>
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+          <TypewriterComponent
+            options={{
+              strings: [
+                "Chatbot.",
+                "Photo Generation.",
+                "Blog Writing.",
+                "Mail Writing.",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
-    )
+      </div>
+
+      <div className="">
+      Crie conteúdo usando IA 10 vezes mais rápido.
+      </div>
+    </div>
+  );
 }
